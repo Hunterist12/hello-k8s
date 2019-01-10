952745f6e8ac8131711968abbc6a579f1b008d58
Mason Hunter <masonhunter12@gmail.com>
2019-01-10 12:53:29 -0600

    Add way to deploy tiller into separate namespace

--- scripts/install-tiller.sh
+++ scripts/install-tiller.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "Installing Tiller into namespace: kube-system"
+### WITHOUT SEPARATE NAMESPACE ###
+
+echo "Installing tiller into namespace: kube-system"
 
 echo "Creating service account..."
 kubectl create serviceaccount -n kube-system tiller
 
 echo "Assigning cluster role..."
 kubectl create clusterrolebinding tiller-binding \
-    --clusterrole=cluster-admin \
+    --clusterrole cluster-admin \
     --serviceaccount kube-system:tiller
 
 echo "Initializing..."
@@ -15,3 +17,22 @@ helm init --service-account tiller
 
 echo "Updating chart repositories..."
 helm repo update
+
+###############################################
+
+# ### WITH SEPARATE NAMESPACE ###
+
+# echo "Installing tiller into namespace: tiller-world"
+# kubectl create namespace tiller-world
+
+# echo "Configuring rbac for tiller..."
+# kubectl create -f ./manifests/rbac/tiller.yaml
+
+# echo "Configuring rbac for helm..."
+# kubectl create -f ./manifests/rbac/helm.yaml
+
+# echo "Initializing..."
+# helm init --service-account tiller --tiller-namespace tiller-world
+
+# echo "Updating chart repositories..."
+# helm repo update
